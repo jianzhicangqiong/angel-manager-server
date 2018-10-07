3857a99b4b3db6c6e0e48baa4005a9c3b901e616
Administrator <513125483@qq.com>
2018-10-07 21:13:20 +0800

    修改小问题

---
+++ src/main/java/com/hero/angel/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.hero.angel.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * spring-security配置
+ */
+@Configuration
+public class WebSecurityConfig {
+
+
+
+}
